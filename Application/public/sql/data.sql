36c155864d7dbf532729f0aad80101a1f7fc8a24
Gene Leo <llos@GenedeMacBook.local>
2017-06-05 16:58:42 +0800

    update data.sql file

--- Application/public/sql/data.sql
+++ Application/public/sql/data.sql
@@ -1,65 +1,110 @@
-/*
-Navicat MySQL Data Transfer
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.35)
+# Database: tp31
+# Generation Time: 2017-06-05 08:42:47 +0000
+# ************************************************************
 
-Source Server         : wamp
-Source Server Version : 50714
-Source Host           : localhost:3306
-Source Database       : aliphp
 
-Target Server Type    : MYSQL
-Target Server Version : 50714
-File Encoding         : 65001
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Date: 2017-06-02 08:56:51
-*/
 
-SET FOREIGN_KEY_CHECKS=0;
+# Dump of table tp_projects
+# ------------------------------------------------------------
 
--- ----------------------------
--- Table structure for nts_projects
--- ----------------------------
-DROP TABLE IF EXISTS `nts_projects`;
-CREATE TABLE `nts_projects` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` char(40) NOT NULL COMMENT 'project name',
+DROP TABLE IF EXISTS `tp_projects`;
+
+CREATE TABLE `tp_projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
   `description` text,
-  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
-  `modify_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'last modified time',
-  `show` tinyint(1) DEFAULT '1' COMMENT 'show it or not',
+  `create_at` datetime NOT NULL,
+  `modify_at` datetime NOT NULL,
+  `ishidden` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of nts_projects
--- ----------------------------
-INSERT INTO `nts_projects` VALUES ('11', 'webnavi', '<p>个人简单网址导航页面.</p>\r\n<a class=\"btn btn-sm btn-info\" target=\"_blank\" href=\"http://www.baidu.com\">演示</a>\r\n<a class=\"btn btn-sm btn-success\" target=\"_blank\" href=\"http://www.baidu.com\">GitHub</a>', '2017-06-01 07:51:25', '2017-06-01 15:49:28', '1');
-INSERT INTO `nts_projects` VALUES ('12', 'ddada', 'dasdasda', '2017-06-01 07:54:24', '2017-06-01 07:54:24', '1');
-INSERT INTO `nts_projects` VALUES ('13', 'ddd', 'ddd', '2017-06-01 07:57:53', '2017-06-01 07:57:53', '1');
-INSERT INTO `nts_projects` VALUES ('14', 'dd', 'dd', '2017-06-01 07:57:58', '2017-06-01 07:57:58', '1');
-INSERT INTO `nts_projects` VALUES ('15', 'fwfw', 'fewfwef', '2017-06-01 07:58:03', '2017-06-01 07:58:03', '1');
-INSERT INTO `nts_projects` VALUES ('16', 'fewfewf', 'fwefewf', '2017-06-01 07:58:08', '2017-06-01 07:58:08', '1');
-INSERT INTO `nts_projects` VALUES ('17', 'fewfwef', 'fg3t43t2', '2017-06-01 07:58:14', '2017-06-01 07:58:14', '1');
-INSERT INTO `nts_projects` VALUES ('18', 'f2f', 'sfewfew', '2017-06-01 07:58:18', '2017-06-01 07:58:18', '1');
-INSERT INTO `nts_projects` VALUES ('19', 'f2f32', 'fsfdsfds', '2017-06-01 07:58:24', '2017-06-01 07:58:24', '1');
-INSERT INTO `nts_projects` VALUES ('20', 'fewf', '2fe2e', '2017-06-01 07:58:29', '2017-06-01 07:58:29', '1');
-INSERT INTO `nts_projects` VALUES ('21', 'fewf', 'fewfewf', '2017-06-01 07:58:34', '2017-06-01 07:58:34', '1');
+LOCK TABLES `tp_projects` WRITE;
+/*!40000 ALTER TABLE `tp_projects` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for nts_user
--- ----------------------------
-DROP TABLE IF EXISTS `nts_user`;
-CREATE TABLE `nts_user` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(30) DEFAULT NULL,
-  `name` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `previlege` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '权限',
+INSERT INTO `tp_projects` (`id`, `name`, `description`, `create_at`, `modify_at`, `ishidden`)
+VALUES
+	(1,'213','213','2017-06-05 16:07:10','2017-06-05 16:07:10',0);
+
+/*!40000 ALTER TABLE `tp_projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tp_test
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_test`;
+
+CREATE TABLE `tp_test` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `sex` tinyint(1) DEFAULT '0' COMMENT '0 man, 1 woman',
+  `age` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `tp_test` WRITE;
+/*!40000 ALTER TABLE `tp_test` DISABLE KEYS */;
+
+INSERT INTO `tp_test` (`id`, `name`, `sex`, `age`)
+VALUES
+	(1,'notus',0,18),
+	(2,'bugber',0,20),
+	(3,'haili',1,16),
+	(4,'kari',1,35),
+	(5,'ham',1,16);
+
+/*!40000 ALTER TABLE `tp_test` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tp_users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tp_users`;
+
+CREATE TABLE `tp_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(60) NOT NULL DEFAULT '',
+  `email` varchar(80) NOT NULL DEFAULT '',
+  `privilege` tinyint(4) NOT NULL DEFAULT '1' COMMENT '0 admin, 1 guest',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='管理员和用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of nts_user
--- ----------------------------
-INSERT INTO `nts_user` VALUES ('1', null, 'notus', 'ba00819f263287af1ff0100c5a323355', '1');
+LOCK TABLES `tp_users` WRITE;
+/*!40000 ALTER TABLE `tp_users` DISABLE KEYS */;
+
+INSERT INTO `tp_users` (`id`, `name`, `password`, `email`, `privilege`)
+VALUES
+	(2,'notus','ba00819f263287af1ff0100c5a323355','hehe_xiao@qq.com',1);
+
+/*!40000 ALTER TABLE `tp_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
